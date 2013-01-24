7c808c6a84807462a020b413a5ddb3202e7eb82b
kostja <konstantin@silin.info>
2013-01-24 18:25:05 +0100

    fixed test

--- ch03_employeeservice/src/main/java/com/apress/projpa2/ch03/employeeservice/beans/EmployeeServiceBean.java
+++ ch03_employeeservice/src/main/java/com/apress/projpa2/ch03/employeeservice/beans/EmployeeServiceBean.java
@@ -10,49 +10,51 @@ import javax.persistence.TypedQuery;
 import com.apress.projpa2.ch03.employeeservice.model.Employee;
 
 @Stateless
-public class EmployeeServiceBean implements EmployeeService {
+public class EmployeeServiceBean  implements EmployeeService {
 
-    @PersistenceContext(unitName = "EmployeeService")
-    protected EntityManager em;
+	@PersistenceContext// (unitName = "EmployeeService")
+	protected EntityManager em;
 
-    public EntityManager getEntityManager() {
-        return em;
-    }
+	@Override
+	public EntityManager getEntityManager() {
+		return em;
+	}
 
-    @Override
-    public Employee createEmployee(int id, String name, long salary) {
-        Employee emp = new Employee(id);
-        emp.setName(name);
-        emp.setSalary(salary);
-        getEntityManager().persist(emp);
-        return emp;
-    }
+	 @Override
+	public Employee createEmployee(int id, String name, long salary) {
+		Employee emp = new Employee(id);
+		emp.setName(name);
+		emp.setSalary(salary);
+		getEntityManager().persist(emp);
+		return emp;
+	}
 
-    @Override
-    public void removeEmployee(int id) {
-        Employee emp = findEmployee(id);
-        if (emp != null) {
-            getEntityManager().remove(emp);
-        }
-    }
+	 @Override
+	public void removeEmployee(int id) {
+		Employee emp = findEmployee(id);
+		if (emp != null) {
+			getEntityManager().remove(emp);
+		}
+	}
 
-    @Override
-    public Employee changeEmployeeSalary(int id, long newSalary) {
-        Employee emp = findEmployee(id);
-        if (emp != null) {
-            emp.setSalary(newSalary);
-        }
-        return emp;
-    }
+	 @Override
+	 public Employee changeEmployeeSalary(int id, long newSalary) {
+	 Employee emp = findEmployee(id);
+	 if (emp != null) {
+	 emp.setSalary(newSalary);
+	 }
+	 return emp;
+	 }
 
-    @Override
-    public Employee findEmployee(int id) {
-        return getEntityManager().find(Employee.class, id);
-    }
+	 @Override
+	 public Employee findEmployee(int id) {
+	 return getEntityManager().find(Employee.class, id);
+	 }
 
-    @Override
-    public List<Employee> findAllEmployees() {
-        TypedQuery<Employee> query = getEntityManager().createQuery("SELECT e FROM Employee e", Employee.class);
-        return query.getResultList();
-    }
+	 @Override
+	public List<Employee> findAllEmployees() {
+		TypedQuery<Employee> query = getEntityManager().createQuery(
+				"SELECT e FROM Employee e", Employee.class);
+		return query.getResultList();
+	}
 }
--- ch03_employeeservice/src/test/java/com/apress/projpa2/ch03/employeeservice/EmployeeServiceTest.java
+++ ch03_employeeservice/src/test/java/com/apress/projpa2/ch03/employeeservice/EmployeeServiceTest.java
@@ -1,6 +1,8 @@
 package com.apress.projpa2.ch03.employeeservice;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import javax.transaction.UserTransaction;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -9,80 +11,103 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 
-import com.apress.projpa2.ch03.employeeservice.beans.EmployeeServiceBean;
+import com.apress.projpa2.ch03.employeeservice.beans.EmployeeService;
 import com.apress.projpa2.ch03.employeeservice.model.Employee;
 
 /**
  * Unit test for EmployeeServiceBean
  *
- * Run with Maven:
- * $ mvn test -Parq-remote
+ * Run with Maven: $ mvn test -Parq-remote
  *
  */
 @RunWith(Arquillian.class)
 public class EmployeeServiceTest {
 
-    @Deployment
-    public static Archive<?> createTestArchive() {
-        return ShrinkWrap
-                .create(WebArchive.class, "employeeServiceTest.war")
-                .addPackages(true, "com.apress.projpa2.ch03.employeeservice")
-                .addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
-                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
-    }
+	@Deployment
+	public static Archive<?> createTestArchive() {
+		return ShrinkWrap
+				.create(WebArchive.class, "employeeServiceTest.war")
+				.addPackages(true, "com.apress")
+				.addAsResource("META-INF/persistence.xml",
+						"META-INF/persistence.xml")
+				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
 
-    @Inject
-    protected Logger log;
+	@Inject
+	protected Logger log;
 
-    @Inject
-    protected EmployeeServiceBean service;
+	@Inject
+	protected EmployeeService service;
 
-    protected Employee testEmployee;
+	@Inject
+	UserTransaction tx;
 
-    @Before
-    public void setup() {
-//        testEmployee = service.createEmployee(10, "John Doe", 10000);
-    }
+	protected Employee testEmployee;
 
-    @Test
-    public void testCreateEmployee() {
-        Assert.assertTrue(true);
-//        Employee employee = service.createEmployee(11, "Alice Smith", 10000);
-//        Assert.assertTrue("Employee should be created", em.contains(employee));
-    }
+	@PostConstruct
+	public void postConstruct() {
+		testEmployee = service.createEmployee(10, "John Doe", 10000);
+		log.info("created employee : {}", testEmployee);
+	}
 
-//    @Test
-//    public void testRemoveEmployee() {
-//        service.removeEmployee(testEmployee.getId());
-//        Assert.assertFalse("Employee should be removed", em.contains(testEmployee));
-//    }
-//
-//    @Test
-//    public void testFindEmployee() {
-//        Employee employee = service.findEmployee(testEmployee.getId());
-//        Assert.assertEquals("Should find the expected employee", testEmployee, employee);
-//    }
-//
-//    @Test
-//    public void testFindAllEmployees() {
-//        Employee employee = service.createEmployee(12, "Bob", 11000);
-//        List<Employee> allEmployees = service.findAllEmployees();
-//        Assert.assertEquals("Should find the expected number of employees", 2, allEmployees.size());
-//        Assert.assertTrue("Should find the expected employee", allEmployees.contains(testEmployee));
-//        Assert.assertTrue("Should find the expected employee", allEmployees.contains(employee));
-//    }
-//
-//    @Test
-//    public void testRaiseEmployeeSalary() {
-//        long raise = 1000L;
-//        long old_salary = testEmployee.getSalary();
-//        Employee employee = service.changeEmployeeSalary(testEmployee.getId(), raise);
-//        Assert.assertEquals(old_salary + raise, employee.getSalary());
-//    }
+	@Test
+	public void testCreateEmployee() throws Exception {
+		Assert.assertTrue(true);
+		tx.begin();
+		Employee employee = service.createEmployee(11, "Alice Smith", 10000);
+		log.info("created employee : {}", employee);
+		Assert.assertTrue("Employee should be created", service
+				.getEntityManager().contains(employee));
+		tx.commit();
+	}
+
+	@Test
+	public void testRemoveEmployee() throws Exception {
+		tx.begin();
+		testEmployee = service.findEmployee(10);
+		log.info("removing employee : {}", testEmployee);
+		service.removeEmployee(testEmployee.getId());
+		tx.commit();
+		tx.begin();
+		Assert.assertFalse("Employee should be removed", service
+				.getEntityManager().contains(testEmployee));
+		tx.commit();
+	}
+
+	// @Test
+	public void testFindEmployee() {
+		Employee employee = service.findEmployee(testEmployee.getId());
+		Assert.assertNotNull(employee);
+		// Assert.assertEquals("Should find the expected employee",
+		// testEmployee,
+		// employee);
+	}
+
+	// @Test
+	public void testFindAllEmployees() {
+		// Employee employee = service.createEmployee(12, "Bob", 11000);
+		// List<Employee> allEmployees = service.findAllEmployees();
+		// Assert.assertEquals("Should find the expected number of employees",
+		// 2,
+		// allEmployees.size());
+		// Assert.assertTrue("Should find the expected employee",
+		// allEmployees.contains(testEmployee));
+		// Assert.assertTrue("Should find the expected employee",
+		// allEmployees.contains(employee));
+	}
+
+	// @Test
+	public void testRaiseEmployeeSalary() {
+		// long raise = 1000L;
+		// long old_salary = testEmployee.getSalary();
+		// Employee employee =
+		// service.changeEmployeeSalary(testEmployee.getId(),
+		// raise);
+		// Assert.assertEquals(old_salary + raise, employee.getSalary());
+	}
 
 }
